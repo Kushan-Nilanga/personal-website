bcfb69520b0407e6445068b4ea9a72104673b3e0
Kushan <dknathalage@gmail.com>
2019-11-23 21:37:33 +1100

    Code to fetch github link, desc and language

--- personal-api-scraper/scrape_github.js
+++ personal-api-scraper/scrape_github.js
@@ -1,18 +1,36 @@
 const axios = require('axios')
+axios_repo = require('axios')
 const cheerio = require('cheerio')
+cheerio_repo = require('cheerio')
 const str = require('string')
 const url = "https://github.com/Kushan-Nilanga?tab=repositories"
 
 axios(url)
     .then(response => {
-        const html = response.data;
-        const $ = cheerio.load(html);
-        const statsTable = $("#user-repositories-list > ul > .public > div > div > h3");
-        console.log(statsTable.length)
+        const html = response.data
+        const $ = cheerio.load(html)
+        const repo_urls = $("#user-repositories-list > ul > .public");
 
-        statsTable.each(function(){
-            const link = $(this).find('a').text().trim()
-            console.log("https://github.com/Kushan-Nilanga/"+link)
+        repo_urls.each(function(){
+            repo_link = $(this).find('h3>a').text().trim()
+            repo_link = "https://github.com/Kushan-Nilanga/"+repo_link
+            console.log(repo_link)
+
+            repo_lang = $(this).find('p').text().trim()
+            console.log(repo_lang)
+
+            repo_desc = $(this).find('.repo-language-color').next().text().trim()
+            console.log(repo_desc)
+
+            // Add the functionality to get read me details 
+            // but not necessory
+            /*
+            axios_repo(repo).then(response_repo=>{
+                const html_repo = response_repo.data
+                const $_repo = cheerio_repo.load(html_repo)      
+            })
+            */
+           console.log("")
         })
     })
     .catch(console.error);
