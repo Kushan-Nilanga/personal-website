681a34e90eb8dc406b291271b49af3e54a85903e
Kushan <dknathalage@gmail.com>
2019-11-24 13:31:20 +1100

    finalised data fetch code
    (However, I discovered that we can set up cron job in strapi itself)

--- personal-api-scraper/scrape_github.js
+++ personal-api-scraper/scrape_github.js
@@ -3,34 +3,54 @@ axios_repo = require('axios')
 const cheerio = require('cheerio')
 cheerio_repo = require('cheerio')
 const str = require('string')
+
+// URL of my github profile
 const url = "https://github.com/Kushan-Nilanga?tab=repositories"
 
+// Scraping
 axios(url)
     .then(response => {
         const html = response.data
         const $ = cheerio.load(html)
+
+        // Fetching data on user repos
         const repo_urls = $("#user-repositories-list > ul > .public");
 
         repo_urls.each(function(){
+
+            // By the navigation link the repo name can be extracted
             repo_link = $(this).find('h3>a').text().trim()
+            console.log(repo_link)
+            
+            // The github link is made here
             repo_link = "https://github.com/Kushan-Nilanga/"+repo_link
             console.log(repo_link)
 
+            // The repository description
             repo_lang = $(this).find('p').text().trim()
             console.log(repo_lang)
 
-            repo_desc = $(this).find('.repo-language-color').next().text().trim()
-            console.log(repo_desc)
+            // The main programming language in which the repos are made
+            repo_lang = $(this).find('.repo-language-color').next().text().trim()
+            console.log(repo_lang)
 
-            // Add the functionality to get read me details 
-            // but not necessory
-            /*
-            axios_repo(repo).then(response_repo=>{
-                const html_repo = response_repo.data
-                const $_repo = cheerio_repo.load(html_repo)      
-            })
-            */
-           console.log("")
+            // The number of stars that repository have
+            repo_stars = $(this).find('.octicon-star').parent().text().trim()
+            console.log(repo_stars)
+
+            // The number of forks the repository have
+            repo_forks = $(this).find('.octicon-repo-forked').parent().text().trim()
+            console.log(repo_forks)
+
+            // The time that the repository last updated
+            repo_forks = $(this).find('relative-time').text().trim()
+            console.log(repo_forks)
+
+            // The licensing of the repository
+            repo_law = $(this).find('.octicon-law').parent().text().trim()
+            console.log(repo_law)
+            
+            console.log("")
         })
     })
     .catch(console.error);
